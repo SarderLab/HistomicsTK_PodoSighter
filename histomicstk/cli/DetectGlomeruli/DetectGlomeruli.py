af0690f6f8fcf2805f40bde4e15d0d0e2392bf14
Brendon Lutnick <brendonl@buffalo.edu>
2020-07-24 14:49:14 -0400

    Add files via upload

--- histomicstk/cli/DetectGlomeruli/DetectGlomeruli.py
+++ histomicstk/cli/DetectGlomeruli/DetectGlomeruli.py
@@ -2,6 +2,7 @@ import os
 from histomicstk.cli.utils import CLIArgumentParser
 
 def main(args):
+    print(os.cmd())
     cmd = 'python3 vis.py --model_variant xception_65 --atrous_rates 6 --atrous_rates 12 --atrous_rates 18 --output_stride 16 --decoder_output_stride 4 --checkpoint_dir /home/brendonl/deeplab/glom-models-multiscale-7-16-20 --dataset_dir {} --vis_crop_size {} --wsi_downsample {}'.format(args.inputImageFile, args.patch_size, args.wsi_downsample)
     os.system(cmd)
 
