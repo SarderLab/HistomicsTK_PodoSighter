bec4002e73ca61c948a678a00a2f2313d486d25b
Brendon Lutnick <brendonl@buffalo.edu>
2020-07-27 12:13:29 -0400

    Add files via upload

--- histomicstk/cli/DetectGlomeruli/DetectGlomeruli.py
+++ histomicstk/cli/DetectGlomeruli/DetectGlomeruli.py
@@ -5,7 +5,26 @@ def main(args):
 
     print(os.getcwd())
 
-    cmd = 'python3 ../deeplab/vis.py --model_variant xception_65 --atrous_rates 6 --atrous_rates 12 --atrous_rates 18 --output_stride 16 --decoder_output_stride 4 --checkpoint_dir /home/brendonl/deeplab/glom-models-multiscale-7-16-20/model.ckpt-200000 --dataset_dir {} --vis_crop_size {} --wsi_downsample {}'.format(args.inputImageFile, args.patch_size, args.wsi_downsample)
+    model_file = args.inputModelFile
+    try:
+        model = model_file.split('.meta')
+        assert len(model) == 2
+        model = model[0]
+    except:
+        try:
+            model = model_file.split('.index')
+            assert len(model) == 2
+            model = model[0]
+        except:
+            try:
+                model = model_file.split('.data')
+                assert len(model) == 2
+                model = model[0]
+            except:
+                print('inputModelFile is not valid: exiting...')
+                exit()
+
+    cmd = 'python3 ../deeplab/vis.py --model_variant xception_65 --atrous_rates 6 --atrous_rates 12 --atrous_rates 18 --output_stride 16 --decoder_output_stride 4 --checkpoint_dir {} --dataset_dir {} --vis_crop_size {} --wsi_downsample {} --overlap_num {}'.format(model, args.inputImageFile, args.patch_size, args.wsi_downsample, args.patch_overlap)
     os.system(cmd)
 
 
