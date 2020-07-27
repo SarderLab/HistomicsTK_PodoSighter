1dee85c2bddff0d8c90a2968380222ab41f117aa
Brendon Lutnick <brendonl@buffalo.edu>
2020-07-27 12:15:23 -0400

    Add files via upload

--- histomicstk/cli/DetectGlomeruli/DetectGlomeruli.py
+++ histomicstk/cli/DetectGlomeruli/DetectGlomeruli.py
@@ -21,8 +21,7 @@ def main(args):
                 assert len(model) == 2
                 model = model[0]
             except:
-                print('inputModelFile is not valid: exiting...')
-                exit()
+                model = model_file
 
     cmd = 'python3 ../deeplab/vis.py --model_variant xception_65 --atrous_rates 6 --atrous_rates 12 --atrous_rates 18 --output_stride 16 --decoder_output_stride 4 --checkpoint_dir {} --dataset_dir {} --vis_crop_size {} --wsi_downsample {} --overlap_num {}'.format(model, args.inputImageFile, args.patch_size, args.wsi_downsample, args.patch_overlap)
     os.system(cmd)
