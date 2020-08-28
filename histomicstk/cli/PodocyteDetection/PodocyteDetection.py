db1bbfc298b03ac05de22f5db0bb89dc033a521f
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2020-08-28 19:38:24 -0400

    Update PodocyteDetection.py

--- histomicstk/cli/PodocyteDetection/PodocyteDetection.py
+++ histomicstk/cli/PodocyteDetection/PodocyteDetection.py
@@ -2,7 +2,7 @@ import os
 from histomicstk.cli.utils import CLIArgumentParser
 
 def main(args):
-    cmd = "python3 ../plugin_podfolder/pod_plugin4.py --inputPASsvsname '{}' --inputIFsvsname '{}' --inputPASxmlname '{}' --orig_IF_Thre {} --TransXY {}".format(args.inputImageFilePAS, args.inputImageFileIF, args.inputAnnotationFile,args.Podocyte_threshold,args.TranslationXY)
+    cmd = "python3 ../plugin_podfolder/pod_plugin5.py --inputPASsvsname '{}' --inputIFsvsname '{}' --inputPASxmlname '{}' --orig_IF_Thre {} --TransXY {}".format(args.inputImageFilePAS, args.inputImageFileIF, args.inputAnnotationFile,args.Podocyte_threshold,args.TranslationXY)
     os.system(cmd)    
 
 if __name__ == "__main__":
