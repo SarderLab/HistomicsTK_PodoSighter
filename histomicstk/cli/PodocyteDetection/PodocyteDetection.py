bded8a35d15e9f8ccf849ae1eabb9fbc034d7765
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2020-08-31 13:14:39 -0400

    Update PodocyteDetection.py

--- histomicstk/cli/PodocyteDetection/PodocyteDetection.py
+++ histomicstk/cli/PodocyteDetection/PodocyteDetection.py
@@ -2,9 +2,11 @@ import os
 from histomicstk.cli.utils import CLIArgumentParser
 
 def main(args2):
-
-    #cmd = "python3 ../plugin_podfolder/pod_plugin6.py -P '{}' -F '{}' -L '{}' -T {} -Tx {} -Ty {}".format(args.inputImageFilePAS, args.inputImageFileIF, args.inputAnnotationFile,args.Podocyte_threshold,args.TranslationX,args.TranslationY)
-    cmd = "python3 ../plugin_podfolder/pod_plugin6.py -P '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_PAS.svs' -F '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_IF.svs' -L '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_PAS.xml' -T 0.4 -Tx 211 -Ty -375"
+    print("cpython3 ../plugin_podfolder/pod_plugin6.py -P '{}' -F '{}' -L '{}' -T {} -Tx {} -Ty {}".format(args2.inputImageFilePAS, args2.inputImageFileIF, args2.inputAnnotationFile,args2.Podocyte_threshold,args2.TranslationX,args2.TranslationY))
+    print("dpython3 ../plugin_podfolder/pod_plugin6.py -P '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_PAS.svs' -F '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_IF.svs' -L '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_PAS.xml' -T 0.4 -Tx 211 -Ty -375))
+    
+    cmd = "python3 ../plugin_podfolder/pod_plugin6.py -P '{}' -F '{}' -L '{}' -T {} -Tx {} -Ty {}".format(args2.inputImageFilePAS, args2.inputImageFileIF, args2.inputAnnotationFile,args2.Podocyte_threshold,args2.TranslationX,args2.TranslationY)
+    #cmd = "python3 ../plugin_podfolder/pod_plugin6.py -P '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_PAS.svs' -F '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_IF.svs' -L '/mnt/girder_worker/ae5d02f4c97b42a7b227b978472c5cc5/24_PAS.xml' -T 0.4 -Tx 211 -Ty -375"
     os.system(cmd)    
 
 if __name__ == "__main__":
