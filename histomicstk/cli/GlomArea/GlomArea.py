6455a89f03560bfc94fe69060ec59441f32ec9a6
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2020-09-02 12:47:04 -0400

    Update GlomArea.py

--- histomicstk/cli/GlomArea/GlomArea.py
+++ histomicstk/cli/GlomArea/GlomArea.py
@@ -1,9 +1,8 @@
 import os
 from histomicstk.cli.utils import CLIArgumentParser
 
-def main(args2):    
+def main(args2):     
     
-    print("python3 ../plugin_podfolder/glom_plugin7.py -P '{}' -L '{}'".format(args2.inputImageFilePAS, args2.inputAnnotationFile))
     cmd = "python3 ../plugin_podfolder/glom_plugin7.py -P '{}' -L '{}'".format(args2.inputImageFilePAS, args2.inputAnnotationFile)
     os.system(cmd)    
 
