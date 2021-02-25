6fb8477565ec7016269d8a56cabef517b8326932
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2021-02-25 14:46:46 -0500

    Update PodoSighter_pix2pix.py

--- histomicstk/cli/PodoSighter_pix2pix/PodoSighter_pix2pix.py
+++ histomicstk/cli/PodoSighter_pix2pix/PodoSighter_pix2pix.py
@@ -4,7 +4,7 @@ from histomicstk.cli.utils import CLIArgumentParser
 def main(args2):         
     
     #cmd = "python3 ../PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 {} -A7 {} -A8 {} -A9 '{}' -A10 '{}' -A11 {}".format(args2.inputFolder, args2.inputImageFile,args2.inputAnnotationFile1, args2.TrainedGeneratorModel ,args2.TrainedDiscriminatorModel,args2.outputAnnotationFile1, args2.PASnucleiThreshold,args2.gauss_filt_size, args2.Disc_size ,args2.species,args2.stain,args2.gpu_id)
-    cmd = "python3 ../PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 {} -A7 {} -A8 {} -A9 '{}' -A10 '{}' -A11 {} -A12 {} -A13 {} -A14 {}".format(args2.inputFolder, args2.inputImageFile,args2.inputAnnotationFile1, args2.TrainedGeneratorModel ,args2.TrainedDiscriminatorModel,args2.outputAnnotationFile1, args2.PASnucleiThreshold,args2.gauss_filt_size, args2.Disc_size ,args2.species,args2.stain,args2.gpu_id,args2.resolut,args2.sz_thre,args2.watershed_thre)
+    cmd = "python3 ../PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 {} -A7 {} -A8 {} -A9 '{}' -A10 '{}' -A11 {} -A12 {} -A13 {} -A14 {} -A15 '{}'".format(args2.inputFolder, args2.inputImageFile,args2.inputAnnotationFile1, args2.TrainedGeneratorModel ,args2.TrainedDiscriminatorModel,args2.outputAnnotationFile1, args2.PASnucleiThreshold,args2.gauss_filt_size, args2.Disc_size ,args2.species,args2.stain,args2.gpu_id,args2.resolut,args2.sz_thre,args2.watershed_thre,args2.outputAnnotationFile2)
 
     os.system(cmd)    
 
