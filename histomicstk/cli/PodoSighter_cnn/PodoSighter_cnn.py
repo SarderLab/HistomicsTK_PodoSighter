3b6cb22249f02ec83be7d6261a11aff03b1fa60a
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2021-02-27 00:49:45 -0500

    Update PodoSighter_cnn.py

--- histomicstk/cli/PodoSighter_cnn/PodoSighter_cnn.py
+++ histomicstk/cli/PodoSighter_cnn/PodoSighter_cnn.py
@@ -10,7 +10,10 @@ def main(args2):
     sys.path.append("../PodoSighter_cnn_folder/")
     
 
-    cmd = "python3 ../PodoSighter_cnn_folder/Complete_CNN_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 '{}' -A7 '{}' -A8 '{}' -A9 {} -A10 {} -A11 {} -A12 {} -A13 {} -A14 {} -A15 '{}'".format(args2.inputFolder, args2.inputImageFile,args2.inputAnnotationFile1, args2.Model,args2.Modelchkpt,args2.Modelidx, args2.species,args2.stain, args2.outputAnnotationFile1, args2.PASnucleiThreshold,args2.gauss_filt_size, args2.Disc_size,args2.resolut,args2.sz_thre,args2.watershed_thre,args2.outputAnnotationFile2)
+    #cmd = "python3 ../PodoSighter_cnn_folder/Complete_CNN_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 '{}' -A7 '{}' -A8 '{}' -A9 {} -A10 {} -A11 {} -A12 {} -A13 {} -A14 {} -A15 '{}'".format(args2.inputFolder, args2.inputImageFile,args2.inputAnnotationFile1, args2.Model,args2.Modelchkpt,args2.Modelidx, args2.species,args2.stain, args2.outputAnnotationFile1, args2.PASnucleiThreshold,args2.gauss_filt_size, args2.Disc_size,args2.resolut,args2.sz_thre,args2.watershed_thre,args2.outputAnnotationFile2)
+    
+    cmd = "python3 ../PodoSighter_cnn_folder/Complete_CNN_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 '{}' -A7 {} -A8 {} -A9 {} -A10 {} -A11 {} -A12 {} -A13 '{}'".format(args2.inputFolder, args2.inputImageFile,args2.inputAnnotationFile1, args2.Model,args2.Modelchkpt,args2.Modelidx, args2.species, args2.PASnucleiThreshold,args2.gauss_filt_size, args2.Disc_size,args2.resolution,args2.size_thre,args2.watershed_thre,args2.outputAnnotationFile2)
+
     os.system(cmd)    
 
 if __name__ == "__main__":
