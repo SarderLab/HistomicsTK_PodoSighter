37f24bbf361c04d140909695e5c16f7a6e322da2
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2021-06-30 10:22:32 -0400

    Update Complete_Pix2pix_Prediction.py

--- histomicstk/PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py
+++ histomicstk/PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py
@@ -2,7 +2,6 @@
 """
 Created on Mon Feb 22 11:09:00 2021
 Full pix2pix prediction
-
 @author: d8@buffalo.edu
 """
 import sys
@@ -44,6 +43,9 @@ parser.add_argument('-A11','--sz_thre',type = int, metavar = '',required = True,
 parser.add_argument('-A12','--watershed_thre',type = float, metavar = '',required = True,help = 'watershed_thre')
 parser.add_argument('-A13','--jsonout',type = str, metavar = '',required = True,help = 'jsonout')
 parser.add_argument('-A14','--outxml1',type = str, metavar = '',required = True,help = 'outxml1')
+parser.add_argument('-A15','--tissue_thickness',type = float, metavar = '',required = True,help = 'Tissue thickness')#new
+parser.add_argument('-A16','--csvfilename',type = str, metavar = '',required = True,help = 'CSV file name')#new
+
 
 args = parser.parse_args()
 
@@ -61,6 +63,8 @@ gpu_id_use = args.gpu_id
 watershed_dist_thre= args.watershed_thre
 size_thre = args.sz_thre
 resol = args.resolut
+tissue_thickness = args.tissue_thickness#new
+csv_file_name = args.csvfilename#new
 
 print(maintempfolder)
 print(svs_file_name)
@@ -76,6 +80,8 @@ print(watershed_dist_thre)
 print(size_thre)
 print(resol)
 print(args.outxml1)
+print(tissue_thickness)#new
+print(csv_file_name)#new
 
 try:
     if species_name =='human':
@@ -169,7 +175,7 @@ exit_code = call("python3 ../pix2pix/test.py --dataroot "+domABtemp+" --gpu_ids 
 '''==============================='''
 
 resdir_exact = Results_save_folder+Model_majorname+"/test_latest/images/"
-TP_HR= create_podocyte_Outxml_pix2pix(svsfile,xmlfile,crop_size,resdir_exact,PAS_nuc_thre,size_thre,gauss_filt_size,watershed_dist_thre,Disc_size,resol)
+TP_HR= create_podocyte_Outxml_pix2pix(svsfile,xmlfile,crop_size,resdir_exact,PAS_nuc_thre,size_thre,gauss_filt_size,watershed_dist_thre,Disc_size,resol,tissue_thickness,csv_file_name)#new
 
 from skimage import exposure
 
